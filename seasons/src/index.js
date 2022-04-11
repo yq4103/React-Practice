22e65dfbdb7c8990c9c1d20b2233cd365a9cfdfe
Yirong Qin <yq4103@mun.ca>
2022-04-11 00:19:25 -0400

    react state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,16 +5,33 @@ class APP extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
 
-    return <div>Latitude: </div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return (
+      <div>
+        Loading!
+      </div>
+    )
   }
 };
 
