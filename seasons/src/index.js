a1b32abcfb08c101cb70de2195765c3d27e5909a
Yirong Qin <yq4103@mun.ca>
2022-04-11 23:08:33 -0400

    react setstate

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class APP extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null, errorMessage: '' };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() {
-
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
-
+  
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
+  
+    return <Spinner message="Loading location. Please accept the location request."/>;
+  }
 
+  render() {
     return (
       <div>
-        Loading!
+        {this.renderContent()};
       </div>
     )
   }
