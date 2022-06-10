f6ee0af5c612a2e87ee7ab41cee56addc7e26b22
Yirong Qin <yq4103@mun.ca>
2022-06-10 01:19:12 -0400

    usecontext context provider

--- 06-usereducer-starting-project/src/components/Home/Home.js
+++ 06-usereducer-starting-project/src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 import classes from "./Home.module.css";
+import AuthContext from "../../context/auth-context";
 
-const Home = (props) => {
+const Home = () => {
+  const authContext = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Log Out</Button>
+      <Button onClick={authContext.onLogout}>Log Out</Button>
     </Card>
   );
 };
--- 06-usereducer-starting-project/src/components/Login/Login.js
+++ 06-usereducer-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -24,7 +25,7 @@ const passwordReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
-const Login = (props) => {
+const Login = () => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState("");
@@ -40,6 +41,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authContext = useContext(AuthContext);
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -86,7 +89,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authContext.onLogin(emailState.value, passwordState.value);
   };
 
   return (
