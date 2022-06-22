f1c18952e4a4fb0e36e2a6d3e24e381a0e933253
Yirong Qin <yq4103@mun.ca>
2022-06-21 23:25:59 -0400

    fetching data from api movie app

---
+++ 08-movieAPI-project/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchHandler() {
+    setIsLoading(true);
+    const res = await fetch("https://swapi.py4e.com/api/films/");
+    const data = await res.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
+
+  // function fetchHandler() {
+  //   fetch("https://swapi.py4e.com/api/films/")
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseDate: movieData.release_date,
+  //         };
+  //       });
+  //       setMovies(transformedMovies);
+  //     });
+  // }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
