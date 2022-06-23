f3b0aab04e04838c98ac240f291f5e4eab2b4593
Yirong Qin <yq4103@mun.ca>
2022-06-22 23:42:14 -0400

    post request

--- 08-movieAPI-project/src/App.js
+++ 08-movieAPI-project/src/App.js
@@ -1,27 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchHandler() {
+  const fetchHandler = useCallback(async () => {
     setIsLoading(true);
-    const res = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await res.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null);
+
+    try {
+      const res = await fetch(
+        "https://movieapi-fdb78-default-rtdb.firebaseio.com/movies.json"
+      );
+      if (!res.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await res.json();
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
-  }
+  }, []);
 
   // function fetchHandler() {
   //   fetch("https://swapi.py4e.com/api/films/")
@@ -41,14 +66,37 @@ function App() {
   //     });
   // }
 
+  useEffect(() => {
+    fetchHandler();
+  }, [fetchHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://movieapi-fdb78-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
