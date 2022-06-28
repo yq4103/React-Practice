4a7e9247ae4b3d94371a0d72ce76e2175f019cf2
Yirong Qin <yq4103@mun.ca>
2022-06-27 22:24:09 -0400

    useInput custom hook in a new form

--- 10-user-input-project/src/components/BasicForm.js
+++ 10-user-input-project/src/components/BasicForm.js
@@ -1,22 +1,103 @@
+import { useState, useEffect } from "react";
+import useInput from "../hooks/use-input";
+
+const inputNotEmpty = (value) => value.trim() !== "";
+const inputIsEmail = (value) => value.includes("@");
+
 const BasicForm = (props) => {
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const {
+    value: enteredFirst,
+    isValid: enteredFirstIsValid,
+    valueIsInvalid: firstIsInvalid,
+    valueChangeHandler: firstChangeHandler,
+    inputBlurHandler: firstBlurHandler,
+    reset: resetName,
+  } = useInput(inputNotEmpty);
+  const {
+    value: enteredLast,
+    isValid: enteredLastIsValid,
+    valueIsInvalid: lastIsInvalid,
+    valueChangeHandler: lastChangeHandler,
+    inputBlurHandler: lastBlurHandler,
+    reset: resetLast,
+  } = useInput(inputNotEmpty);
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    valueIsInvalid: emailIsInvalid,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(inputIsEmail);
+
+  useEffect(() => {
+    if (enteredFirstIsValid && enteredLastIsValid && enteredEmailIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredFirstIsValid, enteredLastIsValid, enteredEmailIsValid]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!enteredFirstIsValid || !enteredLastIsValid || !enteredEmailIsValid) {
+      return;
+    }
+
+    resetName();
+    resetLast();
+    resetEmail();
+  };
+
+  const firstInputClasses = firstIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+  const lastInputClasses = lastIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+  const EmailInputClasses = emailIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className={firstInputClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={firstChangeHandler}
+            onBlur={firstBlurHandler}
+            value={enteredFirst}
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastInputClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={lastChangeHandler}
+            onBlur={lastBlurHandler}
+            value={enteredLast}
+          />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={EmailInputClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
