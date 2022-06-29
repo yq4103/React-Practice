c4be449976491272f73336c4526bc73d6538fd3c
Yirong Qin <yq4103@mun.ca>
2022-06-29 16:37:30 -0400

    accepting user input to firebase

--- 11-foordOrder-HTTP-Forms-project/src/components/Cart/Checkout.js
+++ 11-foordOrder-HTTP-Forms-project/src/components/Cart/Checkout.js
@@ -45,6 +45,13 @@ const Checkout = (props) => {
     if (!formIsValid) {
       return;
     }
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostal,
+    });
   };
 
   return (
