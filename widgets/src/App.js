98a6edcb2f1d0bd03dee3d6a4461f5822d9053a5
Yirong Qin <yq4103@mun.ca>
2022-08-01 19:09:57 -0400

    dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -17,10 +18,31 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "Color red",
+    value: "red",
+  },
+  {
+    label: "Color green",
+    value: "green",
+  },
+  {
+    label: "Color blue",
+    value: "blue",
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (e) => {
+      if (ref.current.contains(e.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+
+    document.body.addEventListener("click", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form" ref={ref}>
+      <div className="field">
+        <label className="label">Select a color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+      <h3 style={{ color: selected.value }}>
+        {selected ? `This text is ${selected.value}` : ""}
+      </h3>
+    </div>
+  );
+};
+
+export default Dropdown;
