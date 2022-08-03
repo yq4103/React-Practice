82998ecd4cf54a867076eed8214a6caf537af7bd
Yirong Qin <yq4103@mun.ca>
2022-08-03 00:38:14 -0400

    translate widget search debounce

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -3,6 +3,16 @@ import axios from "axios";
 
 const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 5000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
 
   useEffect(() => {
     const doTranslation = async () => {
@@ -11,7 +21,7 @@ const Convert = ({ language, text }) => {
         {},
         {
           params: {
-            q: text,
+            q: debouncedText,
             target: language.value,
             key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms - IwDlM",
           },
@@ -20,7 +30,7 @@ const Convert = ({ language, text }) => {
       setTranslated(data.data.translations[0].translatedText);
     };
     doTranslation();
-  }, [language, text]);
+  }, [language, debouncedText]);
   return (
     <div>
       <h1 className="ui header">{translated}</h1>
